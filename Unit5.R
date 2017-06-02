d21ff0ff89416c885813cca0cc5c04fe15c45540
mayanksingh09 <mayank.jan1995@gmail.com>
2017-06-02 18:08:07 +0530

    Unit 5 Analytics Edge

---
+++ Unit5.R
@@ -0,0 +1,11 @@
+#UNIT 5
+
+##Natural Language Processing - How computers understand text
+##Goal: To understand and derive meaning from Human language
+
+##Sentiment mining
+##Amazon Mechanical Turk
+
+
+tweets <- read.csv("./data/tweets.csv")
+
