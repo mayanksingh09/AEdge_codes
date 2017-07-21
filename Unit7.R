d3c9a3fc16d32eb0a9aec3b946c26fa4e188fe69
mayanksingh09 <mayank.jan1995@gmail.com>
2017-07-22 00:24:22 +0530

    Unit7 Analytics Edge

--- Unit7.R
+++ Unit7.R
@@ -64,3 +64,41 @@ ggplot(WHO, aes(x = log(FertilityRate), y = Under15)) + geom_point() + stat_smoo
 ggplot(WHO, aes(x = FertilityRate, y = Under15, color = Region)) + geom_point() + scale_color_brewer(palette="Dark2")
 
 
+# PREDICTIVE POLICING
+
+mvt <- read.csv("./data/mvt.csv", stringsAsFactors = F)
+str(mvt)
+mvt$Date <- strptime(mvt$Date, format = "%m/%d/%y %H:%M")
+
+mvt$Weekday <- weekdays(mvt$Date)
+mvt$Hour <- mvt$Date$hour
+
+WeekdayCounts <- as.data.frame(table(mvt$Weekday))
+str(WeekdayCounts)
+
+library(ggplot2)
+ggplot(WeekdayCounts, aes(x = Var1, y = Freq)) + geom_line(aes(group = 1))
+
+WeekdayCounts$Var1 <- factor(WeekdayCounts$Var1, ordered = TRUE, levels = c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"))
+
+ggplot(WeekdayCounts, aes(x = Var1, y = Freq)) + geom_line(aes(group = 1), linetype = 2, alpha = 0.3) + xlab("Day of the Week") + ylab("Total Motor Vehicle Thefts")
+
+## Heat Map
+DayHourCounts <- as.data.frame(table(mvt$Weekday, mvt$Hour))
+
+str(DayHourCounts)
+DayHourCounts$Hour <- as.numeric(as.character(DayHourCounts$Var2))
+
+### One line each for each day of the week
+ggplot(DayHourCounts, aes(x = Hour, y = Freq)) + geom_line(aes(group= Var1, color = Var1), size = 2)
+
+
+### Creating heatmap
+
+DayHourCounts$Var1 <- factor(DayHourCounts$Var1, ordered = TRUE, levels = c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"))
+
+ggplot(DayHourCounts, aes(x = Hour, y = Var1)) + geom_tile(aes(fill = Freq)) + scale_fill_gradient(name = "Total MV Theft", low = "white", high = "red") +theme(axis.title.y = element_blank())
+
+
+## MAP PLOTS
+
